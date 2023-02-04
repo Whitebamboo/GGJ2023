27a1a4344e749667b93d5d66aca1f3b8393874a5
Whitebamboo <xiunany@andrew.cmu.edu>
2023-02-03 23:19:50 -0500

    upload scripts

---
+++ Assets/Scripts/TreeAttackModule.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAttackModule : MonoBehaviour
+{
+    public void ProcessTreeNodes(List<TreeNode> nodes)
+    {
+
+    }
+}
