eb78fd7dfbf1f5cdb4356e5ae50f8e7a6491cf1e
Chia-Wei Liu <chiawei.liu.1201@gmail.com>
2023-02-04 12:09:02 -0500

    Add Enemy Manager

---
+++ Assets/Scripts/Enemies/WaveInfo.cs
@@ -0,0 +1,10 @@
+﻿
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "WaveInfo", menuName = "ScriptableObjects/WaveInfo", order = 2)]
+    public class WaveInfo : ScriptableObject
+    {
+        public List<SpawnInfo> spawnInfos;
+        public float time;
+    }
